f89be6382495a36861a0b512e55cc5b53004b1dc
“Debashis <debashis.deep205@gmail.com>
2024-02-19 23:58:53 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify,render_template
+import json
+import logging
 
 
 obj=Flask(__name__)
@@ -8,6 +10,7 @@ obj=Flask(__name__)
 def welcome():
     return "Welcome to Flask"
 
+'''
 @obj.route('/cal',methods=["GET"])
 def math_operator():
     operation=request.json["operation"]
@@ -23,6 +26,52 @@ def math_operator():
     else:
         result=int(number1)-int(number2)
     return "the operation is {} and the result is {}".format(operation,result)
+'''
+
+
+@obj.route('/cal', methods=["GET"])
+def math_operator():
+    data = request.json
+    operation = data["operation"]
+    number1 = float(data["number1"])
+    number2 = float(data["number2"])
+    
+    if operation == "add":
+        result = number1 + number2
+    elif operation == "multiply":
+        result = number1 * number2
+    elif operation == "divide":
+        result = number1 / number2
+    else:
+        result = number1 - number2
+    
+    return f'operation: {operation}, result: {result}'
+    
+
+
+
+@obj.route('/calculate', methods=['POST','GET'])
+def calculate():
+    if(request.method == "GET"):
+        return render_template('calculator.html')
+    else:
+        number1 = float(request.form['number1'])
+        number2 = float(request.form['number2'])
+        operation = request.form['operation']
+        if operation == 'add':
+            result = number1 + number2
+        elif operation == 'multiply':
+            result = number1 * number2
+        elif operation == 'divide':
+            result = number1 / number2
+        else:
+            result = 'Invalid operation'
+        return render_template('calculator.html', result=result)
+
+
+
+
+
         
     
 
@@ -30,4 +79,6 @@ def math_operator():
 print(__name__)
 
 if __name__ == '__main__':
+    logging.basicConfig(filename="myapp.log", level=logging.INFO)
+
     obj.run()
